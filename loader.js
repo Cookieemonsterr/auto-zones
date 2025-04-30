d64381906dc754e6cd709309edb180b543a897ff
Cookieemonsterr <naheeeelb03@gmail.com>
2025-04-30 20:42:19 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,7 +1,5 @@
-// loader.js
 (function(){
   var s = document.createElement('script');
-  // bust cache on the real script
-  s.src = 'https://cdn.jsdelivr.net/gh/Cookieemonsterr/auto-zones@latest/auto-zones.js?t=' + Date.now();
+  s.src = 'https://cdn.jsdelivr.net/gh/Cookieemonsterr/auto-zones@latest/auto-zones.js?t=' + new Date().getTime();
   document.body.appendChild(s);
 })();
