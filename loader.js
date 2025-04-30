bb5bc519b9c2f4d7cf56d0be7e4a58c35639b2b2
Cookieemonsterr <naheeeelb03@gmail.com>
2025-04-30 20:53:41 +0300

    Delete loader.js

--- loader.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  var s = document.createElement('script');
-  s.src = 'https://cdn.jsdelivr.net/gh/Cookieemonsterr/auto-zones@latest/auto-zones.js?t=' + new Date().getTime();
-  document.body.appendChild(s);
-})();
