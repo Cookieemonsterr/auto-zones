250123b5dcd265bbb3a8cd88ffe140893dac5609
Cookieemonsterr <naheeeelb03@gmail.com>
2025-04-30 20:33:51 +0300

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,5 @@
+(function(){
+  const s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/Cookieemonsterr/auto-zones@latest/auto-zones.js?t=' + Date.now();
+  document.body.appendChild(s);
+})();
